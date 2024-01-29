ea4b4a5a88623fb56b97fdad3ce0124955a5f2d9
MoAly7yaei <moaly7yaei@outlook.com>
2024-01-29 11:11:06 +0400

    Frontend Streamlit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#Under Development
+import streamlit as st
+import requests
+
+st.title('Feelings Analyzer')
+input_text = st.text_area('Write about your feelings..', '')
+if st.button('Submit'):
+    response = requests.get(f"http://localhost:8000/predict?text={input_text}")
+    if response.status_code == 200:
+        prediction = response.json()['prediction']
+        st.write(f"Prediction: {prediction}")
+    else:
+        st.write("Error in prediction")
