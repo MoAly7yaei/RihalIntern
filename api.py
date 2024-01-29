aaedd3cae8180ea33ca30bb5a4afb8f07ee595e8
MoAly7yaei <moaly7yaei@outlook.com>
2024-01-29 11:49:27 +0400

    Show stressed or not insted 1 or 0

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ app = FastAPI()
 @app.get("/predict")
 async def predict(text: str):
     prediction = Model.predecion(text)
-    return {"prediction": int(prediction)}
+    return {"prediction": "Stressed" if prediction == 1 else "Not Stressed"}
 
 
 if __name__ == '__api__':
