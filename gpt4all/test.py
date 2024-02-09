e422484cd8d194af22cfbae6247a33251d7c6337
MoAly7yaei <moaly7yaei@outlook.com>
2024-02-09 15:58:52 +0400

    Testing the model

---
+++ gpt4all/test.py
@@ -0,0 +1,9 @@
+from model import *
+
+model = Model()
+
+print(model.chat(input = "Explain AI to me: "))
+
+print(model.chat(input = "What is the best usecase for it??"))
+
+print(model.chat(input = "What is the best usecase for it??"))
