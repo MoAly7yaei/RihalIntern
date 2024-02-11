db27dd40e3671250e8f8a4936fef8c4a948553e4
MoAly7yaei <moaly7yaei@outlook.com>
2024-02-11 09:57:51 +0400

    Prepare main.py for Docker

--- Stress Detection/main.py
+++ Stress Detection/main.py
@@ -1,12 +1,14 @@
+import os
 import streamlit as st
 import requests
 
 st.title('Stress Detector')
 input_text = st.text_area('Write about your feelings..', '')
 if st.button('Send'):
-    response = requests.get(f"http://localhost:8000/predict?text={input_text}")
+    api_url = f"http://{os.environ['API_HOST']}:{os.environ['API_PORT']}/predict?text={input_text}"
+    response = requests.get(api_url)
     if response.status_code == 200:
         prediction = response.json()['prediction']
-        st.write(f"Reasult: {prediction}")
+        st.write(f"Result: {prediction}")
     else:
         st.write("Error in prediction")
